5f1805110b189cfdcb77760809a8758be78d6039
otmiM <89715432+otmiM@users.noreply.github.com>
2022-09-15 15:14:41 -0400

    Update ACF.py

--- 2022-Otmi-Macroletters/ACF.py
+++ 2022-Otmi-Macroletters/ACF.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python
 #    Bond vector autocorrelation 
 #    Author : Janani Sampath
-#.   Edited by: Mohammed Al Otmi
+#    Edited by: Mohammed Al Otmi
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
